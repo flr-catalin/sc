2670b480b11736a314ef9c853fac128bc17a6c5c
Florea Catalin <flr.catalin@outlook.com>
2021-03-15 19:05:59 +0200

    Refactor DictionaryAPI

--- src/main/java/ro/uvt/info/sc/dictionary/DictionaryAPI.java
+++ src/main/java/ro/uvt/info/sc/dictionary/DictionaryAPI.java
@@ -7,32 +7,84 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-import lombok.AllArgsConstructor;
-import lombok.Setter;
-
-@AllArgsConstructor
+/**
+ * This singleton class provides access to the Dictionary API.
+ * 
+ * @author Catalin Florea
+ */
 public class DictionaryAPI {
 	
+	/** The single instance. */
+	private static DictionaryAPI INSTANCE = null;
+	
+	/** The response cache. */
 	private final Map<String, Integer> responseCache = new HashMap<>();
 	
+	/** The base URI of the API. */
 	private final URI baseUri = URI.create("https://api.dictionaryapi.dev/api/v2/entries/");
 	
-	@Setter private Language language;
-
-	public DictionaryAPI() {
-		this(Language.EnglishUS);
+	/** The dictionary language. */
+	private final Language language;
+	
+	/**
+	 * Private constructor to ensure there is a single instance.
+	 */
+	private DictionaryAPI(Language language) {
+		this.language = language;
 	}
 	
+	/**
+	 * Gets the single instance. 
+	 */
+	public static DictionaryAPI getInstance(Language language) {
+		if (INSTANCE == null) {
+			INSTANCE = new DictionaryAPI(language);
+		}
+		
+		return INSTANCE;
+	}
+
+	/**
+	 * Finds a word in the dictionary using the
+	 * local cache or the API.
+	 * 
+	 * @param word the word to be searched for
+	 * @return the response code of the request
+	 * 
+	 * @throws IOException could not create connection
+	 */
 	public int find(String word) throws IOException {
 		if (responseCache.containsKey(word)) {
-			return responseCache.get(word).intValue();
+			return getResponseFromCache(word);
 		} else {
-			int responseCode = getConnection(word).getResponseCode();
-			responseCache.put(word, new Integer(responseCode));
-			return responseCode;
+			return getResponseFromApi(word);
 		}
 	}
+
+	/**
+	 * Gets the response from the local cache. 
+	 */
+	private int getResponseFromCache(String word) {
+		return responseCache.get(word).intValue();
+	}
 	
+	/**
+	 * Gets the response from the API.
+	 * 
+	 * @throws IOException could not create connection
+	 */
+	private int getResponseFromApi(String word) throws IOException {
+		int responseCode = getConnection(word).getResponseCode();
+		responseCache.put(word, new Integer(responseCode));
+		return responseCode;
+	}
+	
+	/**
+	 * Creates the connection to the Dictionary API.
+	 * <br><code>URI: {baseUri}/{language}/{word}</code>
+	 * 
+	 * @throws IOException could not create connection
+	 */
 	private HttpURLConnection getConnection(String word) throws IOException {
 		StringBuilder specBuilder = new StringBuilder();
 		specBuilder.append(language.getURL()).append(word);
